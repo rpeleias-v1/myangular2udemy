743e6062e2856585d36daa9e650d14ae8706ff0d
Rodrigo Peleias <rodrigo.peleias@preventsenior.com.br>
2016-12-12 16:58:24 -0200

    Directives chapter

--- src/app/stocks.component.ts
+++ src/app/stocks.component.ts
@@ -6,15 +6,41 @@ import { StockService } from './stock.service';
     template: `<h1>Stocks</h1>
     {{title}}
     
-    <ul>
+    <ul [ngStyle]="{'color':myColor, 'font-size': mySize}">
         <li *ngFor="let stock of stocks">
             {{stock}}
         </li>
-    </ul>`
+    </ul>
+    
+    <hr>
+    {{stockMarkets.length}}
+    <ul *ngIf="stockMarkets.length == 4">
+        <li *ngFor="let stockMarket of stockMarkets" >
+            {{stockMarket}}
+        </li>
+    </ul>
+    
+    <hr>
+    
+    <div [ngSwitch]="market">
+        <div *ngSwitchCase="'NYSE'">New York Stock Exchange</div>
+        <div *ngSwitchCase="'LSE'">London Stock Exchange</div>
+        <div *ngSwitchCase="'HKSE'">Hong Kong Stock Exchange</div>
+        <div *ngSwitchCase="'NASQAD'">NASQAD</div>
+        <div *ngSwitchDefault>Could not find a match</div>
+    </div>`
 })
 export class StocksComponent {
+
+    market = 'HKSE';
+    myColor = 'blue';
+    mySize = '200%';
+    
     title = 'List of Stocks: ';    
     stocks;
+    showStockMarket = true;
+
+    stockMarkets = ['NYSE', 'NASDAQ', 'EURONEXT', 'HKSE', 'LSE'];
 
     constructor(stockService: StockService) {
         this.stocks = stockService.getStocks();
